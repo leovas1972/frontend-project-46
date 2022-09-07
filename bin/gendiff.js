1d939db1e06fabe78330da5b1cf2ea224518b11b
Leontev Vasilii <leovas1972@gmail.com>
2022-09-07 11:20:27 +0700

    added arguments and option in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
+import { Command } from 'commander';
 
-const program = new Command;
+const program = new Command();
 
 program
-.version('0.0.1')
-.description('Compares two configuration files and shows a difference.');
-
-
-program.parse();
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .parse(process.argv);
