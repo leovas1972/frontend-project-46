74d4ec918978ce5f973b6e80f5ec09c13a1c9d45
Leontev Vasilii <leovas1972@gmail.com>
2022-09-13 17:11:20 +0700

    file path function, read and type

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import fs from 'fs';
+
+export const buildFullPath = (filename) => path.join(path.resolve(process.cwd()),'/__fixtures__', filename);
+
+export const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+
+export const defineFileType = (filePath) => {
+  const fileType = path.extname(filePath).slice(1);
+  return fileType;
+};
+
+export const parseFile = (fileFormat, data) => {
+  if(fileFormat) return JSON.parse(data);
+};
+
+
+
