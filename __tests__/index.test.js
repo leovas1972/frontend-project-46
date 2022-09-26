acfb0b099c8ff9f6b18c1c7617e04afcb482d79d
Leontev Vasilii <leovas1972@gmail.com>
2022-09-26 10:01:13 +0700

    flat json file comparison test

---
+++ __tests__/index.test.js
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { describe, expect, test } from '@jest/globals';
+import findDifference from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+describe('Test for JSON file', () => {
+  const fileJson1 = getFixturePath('file1.json');
+  const fileJson2 = getFixturePath('file2.json');
+
+  test('testing function findDifference', () => {
+    const actual = findDifference(fileJson1, fileJson2);
+    expect(actual).toEqual({
+      host: 'hexlet.io',
+      timeout: 50,
+      proxy: '123.234.53.22',
+      follow: false,
+    });
+  });
+});
