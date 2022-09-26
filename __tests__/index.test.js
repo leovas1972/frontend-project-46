6a7e96fa9bcc0bf6a1897c489f409e6036326180
Leontev Vasilii <leovas1972@gmail.com>
2022-09-26 11:18:09 +0700

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,16 @@
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
+//import { fileURLToPath } from 'url';
+//import path, { dirname } from 'path';
+import { buildFullPath } from '../src/utils.js'
 import { describe, expect, test } from '@jest/globals';
 import findDifference from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = dirname(__filename);
+//const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe('Test for JSON file', () => {
-  const fileJson1 = getFixturePath('file1.json');
-  const fileJson2 = getFixturePath('file2.json');
+  const fileJson1 = buildFullPath('file1.json');
+  const fileJson2 = buildFullPath('file2.json');
 
   test('testing function findDifference', () => {
     const actual = findDifference(fileJson1, fileJson2);
