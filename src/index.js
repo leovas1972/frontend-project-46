cf16fcaaa6766755c0f58df7da8af56f9119a739
Leontev Vasilii <leovas1972@gmail.com>
2022-10-03 15:21:26 +0700

    change due to added features

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 // import path from 'path';
 import buildTree from './buldTree.js';
 import {
-  buildFullPath, readFile, parseFile,
+  buildFullPath, readFile, defineFileType,
 } from './utils.js';
+import parseData from './parsers.js';
+import format from './formatters/index.js';
 
-const findDifference = (filepath1, filepath2) => {
-  const data1 = parseFile(readFile(buildFullPath(filepath1)));
-  const data2 = parseFile(readFile(buildFullPath(filepath2)));
+const findDifference = (filepath1, filepath2, type = 'stylish') => {
+  const filePath1 = buildFullPath(filepath1);
+  const filePath2 = buildFullPath(filepath2);
+
+  const fileValue1 = readFile(filePath1);
+  const fileValue2 = readFile(filePath2);
+
+  const fileType1 = defineFileType(filepath1);
+  const fileType2 = defineFileType(filepath2);
+
+  const data1 = parseData(fileValue1, fileType1);
+  const data2 = parseData(fileValue2, fileType2);
 
   const tree = buildTree(data1, data2);
-  return tree;
+
+  return format(type, tree);
 };
 
 export default findDifference;
