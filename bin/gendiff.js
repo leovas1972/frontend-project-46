2740b7c9885ed97610290c135c2fe973f2e451ce
Leontev Vasilii <leovas1972@gmail.com>
2022-10-03 14:49:49 +0700

    stylish as the default formatter in the executable

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,8 +9,8 @@ program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(findDifference(filepath1, filepath2));
+    console.log(findDifference(filepath1, filepath2, program.opts().format));
   });
 program.parse(process.argv);
