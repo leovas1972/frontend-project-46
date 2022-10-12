3ebe0cfbfc851a59969755cd594582b840cf6dff
Leontev Vasilii <leovas1972@gmail.com>
2022-10-12 08:24:43 +0700

    Added test json default, string

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,17 @@
 import { test, expect, describe } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import fs from 'fs';
+import { json, yml } from '../__fixtures__/result.js';
 import findDifference from '../src/index.js';
-import { json, stylish, yml } from '../__fixtures__/result.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const stylish = readFile('stylish.txt');
+const plain = readFile('plain.txt');
 
 const fileJson1 = 'file1.json';
 const fileJson2 = 'file2.json';
@@ -9,15 +20,22 @@ const fileYaml2 = 'file2.yml';
 
 const stylishFormat = 'stylish';
 const jsonFormat = 'json';
+const plainFormat = 'plain';
 
-describe('stylish output', () => {
+describe('output formats', () => {
   test('stylish files', () => {
     expect(findDifference(fileJson1, fileJson2, stylishFormat)).toBe(stylish);
   });
-  test('json files ', () => {
+  test('json files default', () => {
+    expect(findDifference(fileJson1, fileJson2)).toBe(stylish);
+  });
+  test('json files string', () => {
     expect(findDifference(fileJson1, fileJson2, jsonFormat)).toBe(json);
   });
   test('yaml files', () => {
     expect(findDifference(fileYaml1, fileYaml2, stylishFormat)).toBe(yml);
+  });
+  test('plain files', () => {
+    expect(findDifference(fileJson1, fileJson2, plainFormat)).toBe(plain);
   });
 });
