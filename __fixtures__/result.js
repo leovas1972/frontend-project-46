c8927f1dc9ce349836b6483c405c910b52118672
Leontev Vasilii <leovas1972@gmail.com>
2022-10-19 11:25:52 +0700

    delited result.js

--- __fixtures__/result.js
+++ __fixtures__/result.js
@@ -1,4 +1,4 @@
-export const stylish = `
+/* export const stylish = `
 {
     common: {
       + follow: false
@@ -42,11 +42,11 @@ export const stylish = `
         }
         fee: 100500
     }
-}`;
+}`; */
 
-export const json = '[{"name":"common","type":"nested","value":[{"name":"follow","type":"added","value":false},{"name":"setting1","type":"unchanged","value":"Value 1"},{"name":"setting2","type":"removed","value":200},{"name":"setting3","type":"changed","value":[true,null]},{"name":"setting4","type":"added","value":"blah blah"},{"name":"setting5","type":"added","value":{"key5":"value5"}},{"name":"setting6","type":"nested","value":[{"name":"doge","type":"nested","value":[{"name":"wow","type":"changed","value":["","so much"]}]},{"name":"key","type":"unchanged","value":"value"},{"name":"ops","type":"added","value":"vops"}]}]},{"name":"group1","type":"nested","value":[{"name":"baz","type":"changed","value":["bas","bars"]},{"name":"foo","type":"unchanged","value":"bar"},{"name":"nest","type":"changed","value":[{"key":"value"},"str"]}]},{"name":"group2","type":"removed","value":{"abc":12345,"deep":{"id":45}}},{"name":"group3","type":"added","value":{"deep":{"id":{"number":45}},"fee":100500}}]';
+//export const json = '[{"name":"common","type":"nested","value":[{"name":"follow","type":"added","value":false},{"name":"setting1","type":"unchanged","value":"Value 1"},{"name":"setting2","type":"removed","value":200},{"name":"setting3","type":"changed","value":[true,null]},{"name":"setting4","type":"added","value":"blah blah"},{"name":"setting5","type":"added","value":{"key5":"value5"}},{"name":"setting6","type":"nested","value":[{"name":"doge","type":"nested","value":[{"name":"wow","type":"changed","value":["","so much"]}]},{"name":"key","type":"unchanged","value":"value"},{"name":"ops","type":"added","value":"vops"}]}]},{"name":"group1","type":"nested","value":[{"name":"baz","type":"changed","value":["bas","bars"]},{"name":"foo","type":"unchanged","value":"bar"},{"name":"nest","type":"changed","value":[{"key":"value"},"str"]}]},{"name":"group2","type":"removed","value":{"abc":12345,"deep":{"id":45}}},{"name":"group3","type":"added","value":{"deep":{"id":{"number":45}},"fee":100500}}]';
 
-export const yml = `{
+/* export const yml = `{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
@@ -54,3 +54,4 @@ export const yml = `{
   + timeout: 20
   + verbose: true
 }`;
+ */
