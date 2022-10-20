c4d89a9d9dad9c5ed20028027f6c7829707b50ab
Leontev Vasilii <leovas1972@gmail.com>
2022-10-20 21:54:41 +0700

    function code change

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,45 +1,37 @@
 import _ from 'lodash';
 
-const plain = (data) => {
-  const toString = (value) => {
-    const str = typeof value === 'string' ? `'${value}'` : value;
-    return str;
+const stringify = (value) => {
+  if (value === null) {
+    return value;
+  }
+
+  if (typeof value === 'object') {
+    return '[complex value]';
+  }
+
+  if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return String(value);
+};
+
+const plain = (data, ancestors = []) => {
+  const typeMap = {
+    unchanged: () => null,
+    added: ({ value }, path) => `Property '${path.join('.')}' was added with value: ${stringify(value)}`,
+    removed: (none, path) => `Property '${path.join('.')}' was removed`,
+    changed: ({ addedValue, removedValue }, path) => (
+      `Property '${path.join('.')}' was updated. From ${stringify(removedValue)} to ${stringify(addedValue)}`),
+    nested: ({ children }, path) => plain(children, path),
   };
 
-  const iter = (arr, keys) => {
-    const result = arr.map((item) => {
-      if (item.type === 'nested') {
-        return `${iter(item.value, [...keys, item.name])}`;
-      }
+  const result = data.map((node) => {
+    const { key, type } = node;
+    const newAncestors = [...ancestors, key];
+    return typeMap[type](node, newAncestors);
+  });
 
-      if (item.type === 'added') {
-        item.value.toString();
-
-        return _.isPlainObject(item.value) ? `Property '${[...keys, item.name].join('.')}' was added with value: [complex value]` : `Property '${[...keys, item.name].join('.')}' was added with value: ${toString(item.value)}`;
-      }
-
-      if (item.type === 'removed') {
-        return `Property '${[...keys, item.name].join('.')}' was removed`;
-      }
-
-      if (item.type === 'changed') {
-        if (_.isPlainObject(item.value[0])) {
-          return `Property '${[...keys, item.name].join('.')}' was updated. From [complex value] to ${toString(item.value[1])}`;
-        }
-        if (_.isPlainObject(item.value[1])) {
-          return `Property '${[...keys, item.name].join('.')}' was updated. From ${toString(item.value[0])} to [complex value]`;
-        }
-
-        return `Property '${[...keys, item.name].join('.')}' was updated. From ${toString(item.value[0])} to ${toString(item.value[1])}`;
-      }
-      return null;
-    })
-      .filter((element) => element != null)
-      .join('\n');
-
-    return result;
-  };
-  return iter(data, []);
+  return _.compact(_.flatten(result)).join('\n');
 };
 
 export default plain;
