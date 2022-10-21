76262e42348ac04123ec6bbfe3690ac33bb94bee
Leontev Vasilii <leovas1972@gmail.com>
2022-10-21 23:08:58 +0700

    edit path buildFullPath

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 
-export const buildFullPath = (filename) => path.join(path.resolve(process.cwd()), '/__fixtures__', filename);
+export const buildFullPath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
 
 export const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
 
