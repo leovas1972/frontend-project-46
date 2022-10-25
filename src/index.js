0b52e90347d6a1b851aee9e3625187ee3dd52f71
Leontev Vasilii <leovas1972@gmail.com>
2022-10-25 18:21:39 +0700

    implementation of the getData function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-// import path from 'path';
+import path from 'path';
+import fs from 'fs';
 import buildTree from './buldTree.js';
-import {
-  buildFullPath, readFile, defineFileType,
-} from './utils.js';
 import parseData from './parsers.js';
 import format from './formatters/index.js';
 
+const buildFullPath = (filename) => path.resolve(process.cwd(), '__fixtures__', filename);
+
+const readFile = (filePath) => fs.readFileSync(filePath, 'utf-8');
+
+const defineFileType = (filePath) => {
+  const fileType = path.extname(filePath).slice(1);
+  return fileType;
+};
+
+const getData = (filePath) => parseData(readFile(filePath), defineFileType(filePath));
+
 const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-  const filePath1 = buildFullPath(filepath1);
-  const filePath2 = buildFullPath(filepath2);
 
-  const fileValue1 = readFile(filePath1);
-  const fileValue2 = readFile(filePath2);
-
-  const fileType1 = defineFileType(filepath1);
-  const fileType2 = defineFileType(filepath2);
-
-  const data1 = parseData(fileValue1, fileType1);
-  const data2 = parseData(fileValue2, fileType2);
+  const data1 = getData(buildFullPath(filepath1));
+  const data2 = getData(buildFullPath(filepath2));
 
   const tree = buildTree(data1, data2);
 
