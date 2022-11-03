6739391da7d6f164b16fe29685848e99f854723e
Leontev Vasilii <leovas1972@gmail.com>
2022-11-03 07:57:02 +0700

    changing the sequence of checking for an object

--- src/buldTree.js
+++ src/buldTree.js
@@ -3,14 +3,14 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
   return keys.map((key) => {
-    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      return { key, type: 'nested', children: buildTree(data1[key], data2[key]) };
-    }
     if (!_.has(data2, key)) {
       return { key, type: 'removed', value: data1[key] };
     }
     if (!_.has(data1, key)) {
       return { key, type: 'added', value: data2[key] };
+    }
+    if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+      return { key, type: 'nested', children: buildTree(data1[key], data2[key]) };
     }
     if (data1[key] !== data2[key]) {
       return {
